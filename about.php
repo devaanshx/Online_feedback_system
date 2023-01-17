abbf69156ce2cad7a076136f32f44524af65e7e5
Devaansh Singh <57992542+devaanshx@users.noreply.github.com>
2023-01-17 12:51:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">COLLEGE FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Feedback System for College in using PHP 
+               College Feedback System 
+Here we have developed the a faculty feedback system, which is generally used in the college to rate the faculty based on the performance...Here we have 2 modules such as administrator and student.
+
+Administrator is the one who creates the student account by adding all student info and assigning the username and password. 
+Admin also checks the result once all the students have entered the feedback..
+We started the development from the login page, where we have given the option to login as admin and student...Here since we have only one admin account, so no need to create a database to store admin info just select admin in the radio button and login 
+
+You can perform all admin actions such as login to the account and check result..
+
+If you entered the student username and password and selected the student option, then it will show all student information and let you enter the feedback based on the subject.
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
